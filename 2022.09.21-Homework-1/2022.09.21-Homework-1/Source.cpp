7f7a1b637e3ce7a140b57d3a7a931428510234ba
anastasia0378 <113695785+anastasia0378@users.noreply.github.com>
2022-09-21 16:21:14 +0300

    hi

---
+++ 2022.09.21-Homework-1/2022.09.21-Homework-1/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main(int argc, const char* argv[])
+{
+    int n;
+    std::cin >> n;
+    std:: cout << "The next number for the number " << n << " is " << n + 1 << "." << std:: endl;
+    std::cout << "The previous number for the number " << n << " is " << n - 1 << "."<< std:: endl;
+    return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
---
+++ 2022.09.21-Homework-1/task1/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+    std::cout << "The next number for the number " << n << " is " << n + 1 << "." << std::endl;
+    std::cout << "The previous number for the number " << n << " is " << n - 1 << "." << std::endl;
+    return EXIT_SUCCESS;
+}
